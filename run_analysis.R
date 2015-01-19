c92d97676935290114d9fbf3f5df1681b6ce941f
christopherdparker <christopher.d.parker@gmail.com>
2015-01-19 16:42:57 -0500

    Initial setup

---
+++ run_analysis.R
@@ -0,0 +1,17 @@
+# Read in training set
+
+# Read in test set
+
+# Merge the training and data sets
+
+# Extract mean and stardard deviation
+
+# Name activities in the data set
+
+# Label the variables names
+
+# Create a tidy data set with the average of each variable for each activity
+# This will be the submitted version of the data
+
+# Write the data to txt for submission
+write.table(, row.name = FALSE)
